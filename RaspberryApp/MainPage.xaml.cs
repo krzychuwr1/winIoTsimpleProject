7e98c70557dfb52ecc56f31e2080723c5c4ab935
Krzysztof Wrona <krzychuwr1@gmail.com>
2017-04-23 22:47:51 +0200

    Initial commit Added simple project which sends some data to azure

---
+++ RaspberryApp/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.Azure.Devices.Client;
+using System.Text;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace RaspberryApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string iotHubUri = ""; // value from azure iot hub needed
+            string deviceId = ""; // value from azure iot hub needed
+            string deviceKey = ""; // value from azure iot hub needed
+
+            var deviceClient = 
+                DeviceClient.Create(
+                    iotHubUri,
+                    AuthenticationMethodFactory.
+                    CreateAuthenticationWithRegistrySymmetricKey(deviceId, deviceKey),
+                    TransportType.Amqp
+                    );
+
+            var str = Message.Text;
+            var message = new Message(Encoding.ASCII.GetBytes(str));
+            await deviceClient.SendEventAsync(message);        }   
+    }
+}
