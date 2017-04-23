c77d770b61e44a1777ecb02908ee870c54badd25
Krzysztof Wrona <krzychuwr1@gmail.com>
2017-04-23 23:21:17 +0200

    Created a simple website which displays some data from azure iot hub

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string connectionString = ""; //insert connection string from azure iot hub here
+            string iotHubD2cEndpoint = "messages/events";
+
+            var eventHubClient = EventHubClient.
+            CreateFromConnectionString(connectionString, iotHubD2cEndpoint);
+
+            var d2cPartitions = eventHubClient.GetRuntimeInformation().PartitionIds;
+
+            IEnumerable<string> data = new List<string>();
+
+            
+                var receiver = eventHubClient.GetDefaultConsumerGroup().
+                CreateReceiver(d2cPartitions[0], DateTime.Now.AddDays(-1));
+                var dataTask = ReceiveMessagesFromDeviceAsync(receiver);
+                data = dataTask;
+            
+            return View(data);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        IEnumerable<string> ReceiveMessagesFromDeviceAsync(EventHubReceiver receiver)
+        {
+            var data = new List<string>();
+            while (true)
+            {
+                try
+                {
+                    EventData eventData = receiver.Receive(TimeSpan.FromMilliseconds(1000));
+
+                    if (eventData == null) break;
+
+                    string singleEventData = Encoding.UTF8.GetString(eventData.GetBytes());
+                    data.Add(singleEventData);
+                }
+                catch(Exception)
+                {
+                }
+
+            }
+            return data;
+        }
+    }
+}
