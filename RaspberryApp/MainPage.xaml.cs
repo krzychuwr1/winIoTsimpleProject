8e0378aca79f5ff2da82b231d08019cbc3591492
Krzysztof Wrona <krzychuwr1@gmail.com>
2017-04-24 13:10:19 +0200

    Added simple speech recognition for raspberry project

--- RaspberryApp/MainPage.xaml.cs
+++ RaspberryApp/MainPage.xaml.cs
@@ -14,6 +14,11 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Microsoft.Azure.Devices.Client;
 using System.Text;
+using Windows.Media.SpeechRecognition;
+using Windows.ApplicationModel;
+using System.Diagnostics;
+using Windows.Storage;
+using Windows.UI.Core;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -24,9 +29,76 @@ namespace RaspberryApp
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private SpeechRecognizer recognizer;
+
+        private const string SRGS_FILE = "Grammar\\grammar.xml";
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            Unloaded += MainPage_Unloaded;
+
+            initializeSpeechRecognizer();
+
+        }
+
+        private async void initializeSpeechRecognizer()
+        {
+            // Initialize recognizer
+            recognizer = new SpeechRecognizer();
+
+            // Set event handlers
+            recognizer.StateChanged += RecognizerStateChanged;
+            recognizer.ContinuousRecognitionSession.ResultGenerated += RecognizerResultGenerated;
+
+            // Load Grammer file constraint
+            string fileName = String.Format(SRGS_FILE);
+            StorageFile grammarContentFile = await Package.Current.InstalledLocation.GetFileAsync(fileName);
+
+            SpeechRecognitionGrammarFileConstraint grammarConstraint = new SpeechRecognitionGrammarFileConstraint(grammarContentFile);
+
+            // Add to grammer constraint
+            recognizer.Constraints.Add(grammarConstraint);
+
+            // Compile grammer
+            SpeechRecognitionCompilationResult compilationResult = await recognizer.CompileConstraintsAsync();
+
+            Debug.WriteLine("Status: " + compilationResult.Status.ToString());
+
+            // If successful, display the recognition result.
+            if (compilationResult.Status == SpeechRecognitionResultStatus.Success)
+            {
+                Debug.WriteLine("Result: " + compilationResult.ToString());
+
+                await recognizer.ContinuousRecognitionSession.StartAsync();
+            }
+            else
+            {
+                Debug.WriteLine("Status: " + compilationResult.Status);
+            }
+        }
+
+        private void RecognizerResultGenerated(SpeechContinuousRecognitionSession session, SpeechContinuousRecognitionResultGeneratedEventArgs args)
+        {
+            // Output debug strings
+            Debug.WriteLine(args.Result.Status);
+            Debug.WriteLine(args.Result.Text);
+            Dispatcher.RunAsync(CoreDispatcherPriority.Low, () =>
+            {
+                this.Message.Text = args.Result.Text;
+            }
+            );
+        }
+
+        private void RecognizerStateChanged(SpeechRecognizer sender, SpeechRecognizerStateChangedEventArgs args)
+        {
+            Debug.WriteLine("Speech recognizer state: " + args.State.ToString());
+        }
+
+        private async void MainPage_Unloaded(object sender, RoutedEventArgs e)
+        {
+            await recognizer.ContinuousRecognitionSession.StopAsync();
         }
 
         private async void Button_Click(object sender, RoutedEventArgs e)
@@ -45,6 +117,7 @@ namespace RaspberryApp
 
             var str = Message.Text;
             var message = new Message(Encoding.ASCII.GetBytes(str));
-            await deviceClient.SendEventAsync(message);        }   
+            await deviceClient.SendEventAsync(message);
+        }   
     }
 }
